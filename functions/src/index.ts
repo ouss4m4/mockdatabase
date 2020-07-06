c287b0240fc95e56584c4c2db440aec0509da916
Oussama <bz.oussama@gmail.com>
2020-07-06 22:47:59 +0100

    lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -50,55 +50,57 @@ app.post("/users", async (req, res) => {
         `User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`
       );
   }
-  //get all users
-  app.get("/users", async (req, res) => {
-    try {
-      const userQuerySnapshot = await db.collection(userCollection).get();
-      const users: any[] = [];
-      userQuerySnapshot.forEach((doc) => {
-        users.push({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-  //get a single contact
-  app.get("/users/:userId", (req, res) => {
-    const userId = req.params.userId;
-    db.collection(userCollection)
-      .doc(userId)
-      .get()
-      .then((user) => {
-        if (!user.exists) throw new Error("User not found");
-        res.status(200).json({ id: user.id, data: user.data() });
-      })
-      .catch((error) => res.status(500).send(error));
-  });
-
-  // Delete a user
-  app.delete("/users/:userId", (req, res) => {
-    db.collection(userCollection)
-      .doc(req.params.userId)
-      .delete()
-      .then(() => res.status(204).send("Document successfully deleted!"))
-      .catch(function (error) {
-        res.status(500).send(error);
-      });
-  });
-
-  // Update user
-  app.put("/users/:userId", async (req, res) => {
-    await db
-      .collection(userCollection)
-      .doc(req.params.userId)
-      .set(req.body, { merge: true })
-      .then(() => res.json({ id: req.params.userId }))
-      .catch((error) => res.status(500).send(error));
-  });
 });
+
+//get all users
+app.get("/users", async (req, res) => {
+  try {
+    const userQuerySnapshot = await db.collection(userCollection).get();
+    const users: any[] = [];
+    userQuerySnapshot.forEach((doc) => {
+      users.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//get a single contact
+app.get("/users/:userId", (req, res) => {
+  const userId = req.params.userId;
+  db.collection(userCollection)
+    .doc(userId)
+    .get()
+    .then((user) => {
+      if (!user.exists) throw new Error("User not found");
+      res.status(200).json({ id: user.id, data: user.data() });
+    })
+    .catch((error) => res.status(500).send(error));
+});
+
+// Delete a user
+app.delete("/users/:userId", (req, res) => {
+  db.collection(userCollection)
+    .doc(req.params.userId)
+    .delete()
+    .then(() => res.status(204).send("Document successfully deleted!"))
+    .catch(function (error) {
+      res.status(500).send(error);
+    });
+});
+
+// Update user
+app.put("/users/:userId", async (req, res) => {
+  await db
+    .collection(userCollection)
+    .doc(req.params.userId)
+    .set(req.body, { merge: true })
+    .then(() => res.json({ id: req.params.userId }))
+    .catch((error) => res.status(500).send(error));
+});
+
 //define google cloud function name
-export const webApi = functions.https.onRequest(main);
+export const mockdatabase = functions.https.onRequest(main);
